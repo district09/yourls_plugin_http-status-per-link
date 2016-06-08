ead54408a24df595c8bc1fd78c9b02f2869040aa
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2016-06-08 12:53:23 +0200

    Coding standards.

--- plugin.php
+++ plugin.php
@@ -8,6 +8,10 @@ Author: Jelle Sebreghts
 Author URI: https://github.com/Jelle-S
 */
 // No direct call
-if( !defined( 'YOURLS_ABSPATH' ) ) die();
+if (!defined( 'YOURLS_ABSPATH' )) {
+  die();
+}
+
+// Create the plugin.
 include_once 'HTTPStatusPlugin.php';
 new HTTPStatusPlugin(basename(__DIR__));
